26f59fa1b00b3fb4f3c1283f5e3b1bc50289ba86
vominhtaii <kiokunashaki145236789@gmail.com>
2024-12-08 23:46:43 +0700

    update

--- frontend/src/services/ProductService.js
+++ frontend/src/services/ProductService.js
@@ -4,3 +4,7 @@ export const getAllProduct = async () => {
     const res = await axios.get(`${process.env.REACT_APP_API_URL}/product/get-all`);
     return res.data;
 };
+export const createProduct = async(data)=>{
+    const res = await axios.post('$(process.env.REACT_APP_API_URL}/product/create',data)
+    return res.data
+}
