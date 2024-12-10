0b9a24f88c5cc3b47066373e5cd08a7a3798c81b
tan06022004 <522h0012@student.tdtu.edu.vn>
2024-12-10 22:32:24 +0700

    ádf

--- backend/src/routes/UserRouter.js
+++ backend/src/routes/UserRouter.js
@@ -7,9 +7,9 @@ router.post('/sign-up', userController.createUser)
 router.post('/sign-in', userController.loginUser)
 router.post('/log-out', userController.logoutUser)
 router.put('/update-user/:id', userController.updateUser) 
-router.delete('/delete-user/:id', authMiddleWare, userController.deleteUser) 
-router.get('/getAll', authMiddleWare,userController.getAllUser)
-router.get('/get-details/:id', authUserMiddleWare, userController.getAllUser)
+router.delete('/delete-user/:id'/*, authMiddleWare*/, userController.deleteUser) 
+router.get('/getAll'/*, authMiddleWare*/,userController.getAllUser)
+router.get('/get-details/:id'/*, authUserMiddleWare*/, userController.getAllUser)
 router.post('/refresh-token', userController.refreshToken)
 
 module.exports = router
